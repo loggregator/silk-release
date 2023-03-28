2e72f9cf88e5f6b28e64160c59a4a9faf3426bea
Ben Fuller <benjaminf@vmware.com>
2023-03-28 18:24:00 +0000

    add integration testing

--- src/code.cloudfoundry.org/silk-datastore-syncer/main.go
+++ src/code.cloudfoundry.org/silk-datastore-syncer/main.go
@@ -17,22 +17,22 @@ import (
 	"code.cloudfoundry.org/lib/serial"
 )
 
-const (
-	datastoreFilePath  = "/var/vcap/data/container-metadata/store.json"
-	datastoreFileOwner = "vcap"
-	datastoreFileGroup = "vcap"
-)
-
 var (
-	interval      *int
-	gardenNetwork *string
-	gardenAddr    *string
+	interval           *int
+	gardenNetwork      *string
+	gardenAddr         *string
+	silkFile           *string
+	datastoreFileOwner *string
+	datastoreFileGroup *string
 )
 
 func init() {
 	interval = flag.Int("n", 30, "sync interval in seconds.")
 	gardenNetwork = flag.String("gardenNetwork", "", "garden network type.")
 	gardenAddr = flag.String("gardenAddr", "", "garden address.")
+	silkFile = flag.String("silkFile", "", "silk file.")
+	datastoreFileOwner = flag.String("silkFileOwner", "", "owner of silk file")
+	datastoreFileGroup = flag.String("silkFileGroup", "", "group owner of silk file")
 }
 
 func main() {
@@ -44,38 +44,8 @@ func main() {
 	log.Println("gardenAddr:", *gardenAddr)
 
 	gardenClient := client.New(connection.New(*gardenNetwork, *gardenAddr))
-
-	// wait for garden to come alive
-	for {
-		log.Println("Attempting to ping Garden")
-		err := gardenClient.Ping()
-		if err == nil {
-			break
-		}
-		switch err.(type) {
-		case nil:
-			break
-		case garden.UnrecoverableError:
-			log.Fatalln("Garden: unrecoverable:", err)
-		default:
-			log.Println("Garden: cannot connect:", err)
-			time.Sleep(1 * time.Second)
-		}
-	}
-
-	store := &datastore.Store{
-		Serializer: &serial.Serial{},
-		Locker: &filelock.Locker{
-			FileLocker: filelock.NewLocker(datastoreFilePath + "_lock"),
-			Mutex:      new(sync.Mutex),
-		},
-		DataFilePath:    datastoreFilePath,
-		VersionFilePath: datastoreFilePath + "_version",
-		LockedFilePath:  datastoreFilePath + "_lock",
-		FileOwner:       datastoreFileOwner,
-		FileGroup:       datastoreFileGroup,
-		CacheMutex:      new(sync.RWMutex),
-	}
+	WaitForGarden(gardenClient)
+	store := makeDatastore()
 
 	duration := time.Duration(*interval) * time.Second
 	for {
@@ -95,33 +65,16 @@ func main() {
 		}
 
 		for _, c := range gardenContainers {
-			var desiredLogConfig executor.LogConfig
-			props, err := c.Properties()
+			desiredLogConfig, err := getGardenLogConfig(c)
 			if err != nil {
-				log.Printf("Garden container: %s: error retrieving properties: %s", c.Handle(), err.Error())
 				continue
 			}
-			logConfigStr, ok := props["log_config"]
-			if ok {
-				err := json.Unmarshal([]byte(logConfigStr), &desiredLogConfig)
-				if err != nil {
-					log.Printf("Garden container: %s: error unmarshalling container log config from datastore: %s", c.Handle(), err.Error())
-					continue
-				}
-			}
-			log.Printf("Garden container: log config: %#v", desiredLogConfig)
 
 			sc := storeContainers[c.Handle()]
-			var actualLogConfig executor.LogConfig
-			logConfigStr, ok = sc.Metadata["log_config"].(string)
-			if ok {
-				err := json.Unmarshal([]byte(logConfigStr), &actualLogConfig)
-				if err != nil {
-					log.Printf("Datastore container: %s: error unmarshalling container log config from datastore: %s", sc.Handle, err.Error())
-					continue
-				}
+			actualLogConfig, err := getSilkLogConfig(sc)
+			if err != nil {
+				continue
 			}
-			log.Printf("Datastore container: log config: %#v", actualLogConfig)
 
 			if reflect.DeepEqual(desiredLogConfig, actualLogConfig) {
 				log.Println("They are equal.. moving on")
@@ -129,14 +82,85 @@ func main() {
 			}
 
 			log.Printf("Datastore container: %s: reconciling with Garden container", sc.Handle)
-
 			b, err := json.Marshal(desiredLogConfig)
 			if err != nil {
 				log.Printf("Garden container: %s: error marshalling container log config: %s", sc.Handle, err.Error())
 				continue
 			}
 			sc.Metadata["log_config"] = string(b)
-			store.Add(sc.Handle, sc.IP, sc.Metadata)
+			err = store.Add(sc.Handle, sc.IP, sc.Metadata)
+			if err != nil {
+				log.Printf("Error updating log config %s", err.Error())
+			}
 		}
 	}
+}
+
+func WaitForGarden(gardenClient garden.Client) {
+	for {
+		log.Println("Attempting to ping Garden")
+		err := gardenClient.Ping()
+		if err == nil {
+			break
+		}
+		switch err.(type) {
+		case nil:
+			break
+		case garden.UnrecoverableError:
+			log.Fatalln("Garden: unrecoverable:", err)
+		default:
+			log.Println("Garden: cannot connect:", err)
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+}
+func makeDatastore() *datastore.Store {
+	store := &datastore.Store{
+		Serializer: &serial.Serial{},
+		Locker: &filelock.Locker{
+			FileLocker: filelock.NewLocker(*silkFile + "_lock"),
+			Mutex:      new(sync.Mutex),
+		},
+		DataFilePath:    *silkFile,
+		VersionFilePath: *silkFile + "_version",
+		LockedFilePath:  *silkFile + "_lock",
+		FileOwner:       *datastoreFileOwner,
+		FileGroup:       *datastoreFileGroup,
+		CacheMutex:      new(sync.RWMutex),
+	}
+	return store
+}
+
+func getGardenLogConfig(c garden.Container) (executor.LogConfig, error) {
+	props, err := c.Properties()
+	if err != nil {
+		log.Printf("Garden container: %s: error retrieving properties: %s", c.Handle(), err.Error())
+		return executor.LogConfig{}, err
+	}
+	logConfigStr, ok := props["log_config"]
+	var desiredLogConfig executor.LogConfig
+	if ok {
+		err := json.Unmarshal([]byte(logConfigStr), &desiredLogConfig)
+		if err != nil {
+			log.Printf("Garden container: %s: error unmarshalling container log config from datastore: %s", c.Handle(), err.Error())
+			return executor.LogConfig{}, err
+		}
+	}
+	log.Printf("Garden container: log config: %#v", desiredLogConfig)
+	return desiredLogConfig, nil
+}
+
+func getSilkLogConfig(sc datastore.Container) (executor.LogConfig, error) {
+	var actualLogConfig executor.LogConfig
+	logConfigStr, ok := sc.Metadata["log_config"].(string)
+	if ok {
+		err := json.Unmarshal([]byte(logConfigStr), &actualLogConfig)
+		if err != nil {
+			log.Printf("Datastore container: %s: error unmarshalling container log config from datastore: %s", sc.Handle, err.Error())
+			return executor.LogConfig{}, err
+		}
+	}
+	log.Printf("Datastore container: log config: %#v", actualLogConfig)
+	return actualLogConfig, nil
 }
